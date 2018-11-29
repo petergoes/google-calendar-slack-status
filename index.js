2e6a9563cccbee2f687ce7aee481ac6ab1e416a4
Peter Goes <petergoes@gmail.com>
2018-11-29 16:54:49 +0100

    diff more accuratly

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/', (req, res, next) => {
       profile: JSON.stringify({
         "status_emoji": ":no_bell:",
         "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
-        "status_expiration": end.diff(start, 'minutes')
+        "status_expiration": end.diff(start)
       })
     });
   }

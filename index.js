f33e537a9f56e5b088f3ca4cfbf6a09f7b152c39
Peter Goes <petergoes@gmail.com>
2018-12-07 16:41:09 +0100

    readd debug cast to utc

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/', (req, res, next) => {
   const dateFormat = 'MMM D, YYYY [at] hh:mmA';
   const start = moment(req.body.start, dateFormat);
   const end = moment(req.body.end, dateFormat);
-  const endUnix = moment(req.body.end, dateFormat).unix()
+  const endUnix = moment(req.body.end, dateFormat).unix().utc()
   let dnd = dndToken.test(status)
   let dndEndTime;
 
@@ -40,7 +40,7 @@ app.post('/', (req, res, next) => {
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
       "status_emoji": dnd ? ":no_bell:" : ":male-technologist:",
-      "status_text": status,
+      "status_text": `${status} - endUtc ${endUnix}`,
       "status_expiration": `${endUnix}`
     })
   });

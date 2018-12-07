4eb738f213c7b3afe5e31c68bc444256b991a442
Peter Goes <petergoes@gmail.com>
2018-12-07 14:24:58 +0100

    log raw end

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/', (req, res, next) => {
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
       "status_emoji": dnd ? ":no_bell:" :  ":male-technologist:",
-      "status_text": `${status} ${end}`,
+      "status_text": `${status} ${end} ${req.body.end}`,
       // "status_expiration": end
     })
   });
